89fb3e2e4d16cc23e83465c9ee8dd89e9b55e9d7
TotallyNotBiased <hi@jerromeyong.com>
2026-03-23 01:56:39 +1100

    chore: initial commit with rust flake env

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "rust flake template";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          clippy
+          rust-analyzer
+        ];
+        shellHook = "echo 'rust env loaded'";
+      };
+    };
+}
